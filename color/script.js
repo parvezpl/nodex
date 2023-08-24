d3b854133569d89fa6dd75867ad749d629e02d4e
parvez <parvezalam336@gmail.com>
2023-08-24 17:31:05 +0530

    full correct

--- color/script.js
+++ color/script.js
@@ -1,3 +1,20 @@
-const buttons=document.querySelectorAll(".button")
+const buttons=document.querySelectorAll('.button');
+const body = document.querySelector('body');
 
-console.log("Button ")
+buttons.forEach(function(button) {
+    body.addEventListener('click', function(e) {
+        // console.log(e)
+        console.log(e.target.id);
+        if (e.target.id==='grey') {
+            body.style.backgroundColor =e.target.id;}
+        if (e.target.id==='white') {
+            body.style.backgroundColor =e.target.id;}
+        if (e.target.id==='blue') {
+            body.style.backgroundColor =e.target.id;}
+        if (e.target.id==='yellow') {
+            body.style.backgroundColor =e.target.id;}
+        if (e.target.id==='red') {
+            body.style.backgroundColor =e.target.id;}
+        
+})})
+
