9e7b7b7333feb6a13b1376decaa617ad52bcb18b
parvez <parvezalam336@gmail.com>
2023-08-30 14:54:01 +0530

    validattion chanage

--- suduku/validation.js
+++ suduku/validation.js
@@ -70,12 +70,11 @@ cellxy.addEventListener('input', function(e){
     // targetCell(tcellid); // send cell id in function
     cellXYgenrater(tcellid)
     let c=cellWithxy[cellsNubmer.indexOf(tcellid)];
-    // console.log("cell",tcellid,tcellVelue, c);
     xyPositonValue(c.i,c.x, tcellVelue, tcellid,trget);
     for (let numm = 1; numm <=9; numm++) {
         boxNumber(numm);
     }
-        chakeBoxNumber(trget);
+    chakeBoxNumber(trget);
 })
 
 
@@ -89,41 +88,79 @@ function xyPositonValue(x,y, tcellVelue, tcellid, trget) {
     fbR=[]; // for clean record num
     console.log(".........");
 }
+
+
 ///////////////////////////////  up coplited
 
 // down sinde start working ....................
 
 let allCellNumber=[]; // all cells id store 
 let pos=0 ;
+let box1=[],box2=[],box3=[],
+    box4=[],box5=[],box6=[],
+    box7=[],box8=[],box9=[];
+
+
 function chakeBoxNumber(trget) {
-        let rv=allCellNumber.forEach((element) => {
+            allCellNumber.forEach((element) => {
             if (element.id==trget.id) {
                 console.log("match",allCellNumber.indexOf(element));
                 pos=allCellNumber.indexOf(element)
             }
-        });
+            });
 
     if (pos<9) {
-        console.log("box1");
+        console.log("box1------", trget.value);
+        if (!box1.includes(trget.value)) {
+            box1.push(trget.value)
+        } else{ trget.value='';}
+
         // boxNumber(1);
     } else if(pos<18) {
         console.log("box2");
+        console.log("box2------", trget.value);
+        if (!box2.includes(trget.value)) {
+            box2.push(trget.value)
+        } else{ trget.value='';}
     } else if(pos<27) {
         console.log("box3");
+        if (!box3.includes(trget.value)) {
+            box3.push(trget.value)
+        } else{ trget.value='';}
     } else if(pos<36) {
         console.log("box4");
+        if (!box4.includes(trget.value)) {
+            box4.push(trget.value)
+        } else{ trget.value='';}
     }  else if(pos<45) {
         console.log("box5");
+        if (!box5.includes(trget.value)) {
+            box5.push(trget.value)
+        } else{ trget.value='';}
     }  else if(pos<54) {``
         console.log("box6");
+        if (!box6.includes(trget.value)) {
+            box6.push(trget.value)
+        } else{ trget.value='';}
     }  else if(pos<63) {
         console.log("box7");
+        if (!box7.includes(trget.value)) {
+            box7.push(trget.value)
+        } else{ trget.value='';}
     } else if(pos<72) {
         console.log("box8");
+        if (!box8.includes(trget.value)) {
+            box8.push(trget.value)
+        } else{ trget.value='';}
     } else if(pos<81) {
         console.log("box9");
+        console.log("box1------", trget.value);
+        if (!box9.includes(trget.value)) {
+            box9.push(trget.value)
+        } else{ trget.value='';}
     } 
 }
+
 
 let m, l;
 function boxNumber(pr) {
@@ -171,6 +208,46 @@ function boxSelection(x,i, n) {
     if (allCellNumber.length<81) {
         allCellNumber.push(selectCell);
     }
-    selectCell.value =n ;
+    // selectCell.value =n ;
 
 }
+
+////..................................... work in progress ................................
+
+// let listxx=[];
+// function newboxWiseSelectioncell(s,e) {
+//     for (let i = 1; i <=9; i++) {
+//         for (let x = 1;  x <=9;x++) {
+//             listxx.push({i,x});   
+//         }
+//     }
+//     for (let z = s; z < e; z++) { 
+//         for (let v = 0; v <3; v++) {
+//             newboxSelection(listxx[z+v].i, listxx[z+v].x, g);
+//             g+=1;
+//         }
+//         z=z+8;     
+//     }
+// }
+
+function fillBox(params) {
+    for (let i = 1; i <=9; i++) {
+        for (let x = 1;  x <=9;x++) {
+            newboxSelection(x,i,i)
+            // listx.push({i,x});   
+        }
+    }
+
+}
+fillBox();
+
+function newboxSelection(x,i, n) {
+    // console.log(`box ${x}, ${i}, ${n}`);
+    let selectCell=document.querySelector(`#grid > tbody > tr:nth-child(${x}) > td:nth-child(${i})`).lastChild
+    if (allCellNumber.length<81) {
+        allCellNumber.push(selectCell);
+    }
+    selectCell.value =n ;
+}
+
+
